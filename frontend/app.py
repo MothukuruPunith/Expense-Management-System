409c34026b90b88f4f0c1176f0ea5d7cb5597cd5
MothukuruPunith <punithmothukuru970@gmail.com>
2025-12-05 18:09:09 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import requests
 from add_update import add_update
 from analytics_ui_date import analytics
 from analytics_ui_month import analytics_by_month
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://expense-management-system-2jo3.onrender.com/"
 st.title("EXPENSE MANAGEMENT SYSTEM")
 tab1,tab2,tab3 = st.tabs(["Add/Update","Analytics by Date","Analytics by Month"])
 with tab1:
@@ -13,4 +13,5 @@ with tab1:
 with tab2:
     analytics()
 with tab3:
+
     analytics_by_month()
